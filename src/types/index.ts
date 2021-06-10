c77ab18220a03e6284c2f189a79649679e8c4f08
kjmczk <vollgong@gmail.com>
2021-06-10 14:48:00 +0900

    Sort types

--- src/types/index.ts
+++ src/types/index.ts
@@ -3,6 +3,24 @@ import { MagicUserMetadata } from 'magic-sdk';
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 type Ref = any;
 
+export type FaunaToken = {
+  ref: Ref;
+  ts: number;
+  instance: Ref;
+  secret: string;
+};
+
+export type SessionObj = {
+  userInfo: MagicUserMetadata;
+  faunaTokenSecret: FaunaToken['secret'];
+};
+
+export type UserDoc = {
+  ref?: Ref;
+  ts?: number;
+  data?: { email: string };
+};
+
 export type TodoDoc = {
   ref: Ref;
   ts: number;
@@ -15,22 +33,4 @@ export type TodoDoc = {
 
 export type Todos = {
   data: TodoDoc[];
-};
-
-export type UserDoc = {
-  ref?: Ref;
-  ts?: number;
-  data?: { email: string };
-};
-
-export type FaunaToken = {
-  ref: Ref;
-  ts: number;
-  instance: Ref;
-  secret: string;
-};
-
-export type SessionObj = {
-  userInfo: MagicUserMetadata;
-  faunaTokenSecret: string;
 };
